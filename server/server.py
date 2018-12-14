a34925b9683fe59e12f1c871eeb4d1964067013e
AlexVestin <aleve030@student.liu.se>
2018-12-14 23:04:42 +0100

    server

--- server/server.py
+++ server/server.py
@@ -2,15 +2,17 @@ from SimpleWebSocketServ import SimpleWebSocketServer, WebSocket
 import socket
 clients = []
 global_id_counter = 0
+
 class SimpleServer(WebSocket): 
     initialized = False
     name = ""
     id = -1
     is_client = False
+    is_host = False
 
     def send_to_servers(self, msg):
-         for client in clients:
-            if not client.is_client:
+        for client in clients:
+            if not client.is_client and client != self:
                 client.sendMessage(str(chr(self.id) + msg))                
 
     def handleMessage(self):
@@ -19,6 +21,8 @@ class SimpleServer(WebSocket):
             return
         elif "SERVER" in self.data:
             self.is_client = False
+            if not [x for x in clients if x.is_host]:
+                self.is_host = True
             return
         
         # ID handshake
@@ -31,12 +35,21 @@ class SimpleServer(WebSocket):
                 global_id_counter += 1
             else:
                self.send_to_servers(self.data)
+        elif self.is_host:
+            self.send_to_servers(self.data)
 
     def handleConnected(self):
        print(self.address, 'connected')
        clients.append(self)
 
     def handleClose(self):
+       if self.is_host:
+           for c in clients:
+               if not c.is_client:
+                   c.is_host = True
+                   c.sendMessage("HOSTING")
+                   break
+                
        clients.remove(self)
        print(self.address, 'closed')
 
